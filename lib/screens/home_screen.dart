a6baf362082d6ec981c5e6dccdcdf05908532c5b
eddieHdz <eduardo.hdzdelarosa@gmail.com>
2023-03-31 14:24:15 -0600

    feat: se creo un bottomnavigationbar con dos items y un flaotingactionbutton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/widgets/custom_navigatorbar.dart';
+import 'package:qr_scanner/widgets/scan_button.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete_forever))
+        ],
+      ),
       body: Center(
         child: Text('Home screen'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
